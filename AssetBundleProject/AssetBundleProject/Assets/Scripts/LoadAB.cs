45327babf4e3992d3269676192f8b12d311531d6
Baize <baize.hex66ccff@gmail.com>
2018-03-13 16:57:49 +0800

    learn how to load manifest

--- AssetBundleProject/AssetBundleProject/Assets/Scripts/LoadAB.cs
+++ AssetBundleProject/AssetBundleProject/Assets/Scripts/LoadAB.cs
@@ -7,13 +7,15 @@ using UnityEngine.Networking;
 public class LoadAB : MonoBehaviour {
 
     // Use this for initialization
+    string path = "AssetBundles";
     string pathCapsule = "AssetBundles/capsulewall.unity3d";
     string pathShare = "AssetBundles/share.unity3d";
     string capsuleWall = "CapsuleWall";
     void Start () {
+        LoadDependeciesManifest();
         //0
         //AssetBundle ab1 = AssetBundle.LoadFromFile(path);
-        AssetBundle ab2 = AssetBundle.LoadFromFile(pathShare);
+        //AssetBundle ab2 = AssetBundle.LoadFromFile(pathShare);
         //GameObject go1 = ab1.LoadAsset<GameObject>("CapsuleWall");        
         //Instantiate(go1);
         //1
@@ -59,7 +61,7 @@ public class LoadAB : MonoBehaviour {
     }
     IEnumerator LoadUsedWebRequest()
     {
-        string url = "file:///E:/U3D_Workplace/源码工程、PPT/源码工程、PPT/AssetBundleProject/AssetBundleProject/AssetBundles/capsulewall.unity3d";
+        string url = "file:///E:/git_U3D/AssetBundleProject/AssetBundleProject/AssetBundles/capsulewall.unity3d";
         //string url = "http://localhost/AssetBundles/capsulewall.unity3d";
         UnityWebRequest request = UnityWebRequest.GetAssetBundle(url);
         yield return request.SendWebRequest();
@@ -72,8 +74,17 @@ public class LoadAB : MonoBehaviour {
         AssetBundleRequest abRequest = ab.LoadAssetAsync<GameObject>(capsuleWall);        
         yield return abRequest;
         var gameObject = abRequest.asset;
-        Instantiate(gameObject);
-        
-        
+        Instantiate(gameObject);                
+    }
+    private void LoadDependeciesManifest()
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/AssetBundles");
+        AssetBundleManifest manifest = ab.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+        string[] dependencies = manifest.GetAllDependencies("capsulewall.unity3d");
+        foreach (var dependence in dependencies)
+        {
+            Debug.Log(dependence);
+            AssetBundle.LoadFromFile(Path.Combine(path, dependence));
+        }        
     }
 }
